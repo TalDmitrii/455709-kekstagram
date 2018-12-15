5c5b0d011fdc7125350679b0921226100cd263ba
TalDmitrii <30574780+TalDmitrii@users.noreply.github.com>
2018-12-15 16:21:02 +0200

    Работа с сетью#2

--- js/form.js
+++ js/form.js
@@ -18,13 +18,8 @@
 
   var effect = document.querySelector('.effect-level');
   var effectNone = document.querySelector('#effect-none');
-  var effectLevel = effect.querySelector('.effect-level__value');
   var effectHandle = effect.querySelector('.effect-level__pin');
-  var effectLine = effect.querySelector('.effect-level__line');
   var effectLineDepth = effect.querySelector('.effect-level__depth');
-  var isDraggable = false;
-  var cursorStartX;
-  var pinStartX;
 
   // Отправляет данные формы.
   form.addEventListener('submit', function (evt) {
@@ -109,7 +104,7 @@
   // Открывает форму загрузки изображения при наступлении события 'change'.
   uploadFile.addEventListener('change', openUploadForm);
 
-  // Удаляет значение 'input'.
+  // Удаляет значение в поле загрузки файла - 'input'.
   uploadFile.addEventListener('click', function () {
     uploadFile.value = '';
   });
@@ -230,79 +225,4 @@
   textDescription.addEventListener('blur', function () {
     document.addEventListener('keydown', escCloseUploadForm);
   });
-
-  // Добавляет обработчик drug-n-drop.
-  effectHandle.addEventListener('mousedown', function (evt) {
-    isDraggable = true;
-
-    cursorStartX = evt.clientX;
-    pinStartX = evt.target.offsetLeft;
-
-    document.addEventListener('mousemove', setPinPosition);
-    document.addEventListener('mouseup', function () {
-      isDraggable = false;
-      document.removeEventListener('mousemove', setPinPosition);
-    });
-  });
-
-  // Расчитывает и устанавливает положение ползунка при drug-n-drop.
-  // @param {object} moveEvt - Объект event.
-  function setPinPosition(moveEvt) {
-    var sliderWidth = effectLine.clientWidth;
-    var percentSliderWidth = sliderWidth / 100;
-
-    if (moveEvt.buttons === 1 && isDraggable) {
-      var cursorShiftX = moveEvt.clientX - cursorStartX;
-      var pinShiftX = pinStartX + cursorShiftX;
-      var pinShiftPercentX = pinShiftX / percentSliderWidth;
-
-      if (pinShiftPercentX > 100) {
-        pinShiftPercentX = 100;
-      } else if (pinShiftPercentX < 0) {
-        pinShiftPercentX = 0;
-      }
-
-      effectHandle.style.left = pinShiftPercentX + '%';
-      effectLineDepth.style.width = pinShiftPercentX + '%';
-      effectLevel.value = Math.round(pinShiftPercentX);
-
-      setEffectValue(effectLevel.value, currentEffect);
-    }
-  }
-
-  // Устанавливает глубину эффекта.
-  // @param {number} percent - Значение глубины эффекта в процентах.
-  // @param {object} currentEffect - Текущий эффект, к которому применяется изменение глубины.
-  function setEffectValue(percent, effectImage) {
-    if (effectImage.className === 'effects__preview--chrome') {
-      effectImage.style.filter = 'grayscale(' + percent / 100 + ')';
-    } else if (effectImage.className === 'effects__preview--sepia') {
-      effectImage.style.filter = 'sepia(' + percent / 100 + ')';
-    } else if (effectImage.className === 'effects__preview--marvin') {
-      effectImage.style.filter = 'invert(' + percent + '%)';
-    } else if (effectImage.className === 'effects__preview--phobos') {
-      effectImage.style.filter = 'blur(' + percent / 33.3 + 'px)';
-    } else if (effectImage.className === 'effects__preview--heat') {
-      effectImage.style.filter = 'brightness(' + (1 + (percent / 50)) + ')';
-    }
-  }
-
-  // Расчитывает и устанавливает положение ползунка при клике.
-  // @param {object} evt - Объект event.
-  function setSliderPosition(evt) {
-    if (evt.target.className === 'effect-level__line' || evt.target.className === 'effect-level__depth') {
-      var sliderWidth = effectLine.clientWidth;
-      var percentSliderWidth = sliderWidth / 100;
-      var pinPositionInPercent = evt.offsetX / percentSliderWidth;
-
-      effectHandle.style.left = pinPositionInPercent + '%';
-      effectLineDepth.style.width = pinPositionInPercent + '%';
-      effectLevel.value = Math.round(pinPositionInPercent);
-
-      setEffectValue(effectLevel.value, currentEffect);
-    }
-  }
-
-  // Устанавливает положение ползунка и соответствующую глубину эффекта.
-  effectLine.addEventListener('click', setSliderPosition);
 })();
